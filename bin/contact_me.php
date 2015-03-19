74436fac0ec0b99b2c3366e980d4c19363a559f6
MichelleMcC <michelletmccarthy1@gmail.com>
2015-03-19 16:43:43 +0000

    Updated About

--- bin/contact_me.php
+++ bin/contact_me.php
@@ -17,7 +17,7 @@ $message = $_POST['message'];
 	
 // create email body and send it	
 $to = 'taxibase@corkairporttaxi.ie'; // PUT YOUR EMAIL ADDRESS HERE
-$email_subject = "Modern Business Contact Form:  $name"; // EDIT THE EMAIL SUBJECT LINE HERE
+$email_subject = "Cork Airport Taxi:  $name"; // EDIT THE EMAIL SUBJECT LINE HERE
 $email_body = "You have received a new message from your website's contact form.\n\n"."Here are the details:\n\nName: $name\n\nPhone: $phone\n\nEmail: $email_address\n\nMessage:\n$message";
 $headers = "From: noreply@your-domain.com\n";
 $headers .= "Reply-To: $email_address";	
