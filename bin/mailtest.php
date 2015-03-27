e8e9fe1829d99ff8040aa3ea4955aac0af001213
MichelleMcC <michelletmccarthy1@gmail.com>
2015-03-27 08:58:58 +0000

    Updated files

---
+++ bin/mailtest.php
@@ -0,0 +1,4 @@
+<?php
+mail('michelletmccarthy1.com','Test mail','The mail function is working!');
+echo 'Mail sent!';
+?>
--- js/contact_me.js
+++ js/contact_me.js
@@ -27,7 +27,7 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "./bin/contact_me.php",
+                url: "./corkairporttaxi/bin/contact_me.php",
                 type: "POST",
                 data: {
                     name: name,
